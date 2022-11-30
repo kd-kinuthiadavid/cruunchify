88dd44ce71dea7fe41cd0bdf7fe295d9c35e4a55
KINUTHIA DAVID <kd.kinuthiadavid@gmail.com>
2022-11-30 10:54:19 +0300

    chore(build): configure react-query devtools

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,23 @@
 import '../styles/globals.css'
 import type { AppProps } from "next/app";
+import { useState } from "react";
 import {
   Hydrate,
   QueryClient,
   QueryClientProvider,
 } from "@tanstack/react-query";
-import { useState } from "react";
-
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  // adapted from https://tanstack.com/query/v4/docs/guides/ssr
   const [queryClient] = useState(() => new QueryClient());
 
   return (
     <QueryClientProvider client={queryClient}>
       <Hydrate state={pageProps.dehydratedState}>
         <Component {...pageProps} />
+        {/* adapted from https://tanstack.com/query/v4/docs/devtools */}
+        <ReactQueryDevtools initialIsOpen={false} />
       </Hydrate>
     </QueryClientProvider>
   );
