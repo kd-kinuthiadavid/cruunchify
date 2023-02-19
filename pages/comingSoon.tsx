612c1f355fca56034d899c8c6ddf8b304a9d15da
KINUTHIA DAVID <kd.kinuthiadavid@gmail.com>
2023-02-19 12:05:56 +0300

    feat: create a coming-soon page

---
+++ pages/comingSoon.tsx
@@ -0,0 +1,28 @@
+import { useRouter } from "next/router";
+import React from "react";
+
+const comingSoon = () => {
+  const router = useRouter();
+  return (
+    <section className="flex gap-y-20 flex-col justify-center items-center h-full">
+      <h1 className="font-black text-8xl md:text-9xl">Coming Soon!</h1>
+      <div className="flex flex-col items-center gap-y-10">
+        <p className="text-4xl max-w-4xl font-thin leading-relaxed">
+          Psst ... guess what? This feature is still cooking up in the lab.
+          We're working hard to bring this and other innovative features that
+          will take your experience to the next level. Thanks for your patience.
+        </p>
+        <button
+          className="flex justify-center items-center bg-cr-light-green px-10 py-5 gap-x-5 rounded-md mb-10 lg:max-w-lg"
+          onClick={() => router.push("/dashboard")}
+        >
+          <small className="text-lg lg:text-xl font-semibold capitalize">
+            Go Back Home
+          </small>
+        </button>
+      </div>
+    </section>
+  );
+};
+
+export default comingSoon;
